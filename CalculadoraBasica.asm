9e8f92408677c3458375d77c3e11c2b40af3c626
unknown <lauromanuelgarciaarzapalo@gmail.com>
2020-05-27 14:14:23 -0500

    Resta, suma y finaliza programa

--- CalculadoraBasica.asm
+++ CalculadoraBasica.asm
@@ -2,7 +2,7 @@
 ;Asignatura: Lenguajes y automatas ll
 
 ;Nombre: Geovani Alberto Tuz Poot
-;        Lauro Garcia
+;        Lauro Manuel Garcia Arzapalo
 
 
 ;Actividad: Calculadora
@@ -156,3 +156,163 @@ division:
     cmp al,27; ascii de ESC
     je Menu
          
+resta:   
+    mov ah,09
+    lea dx,textoresta;impresion de cadena y se detiene cuando detecta el --> $
+    int 21h
+           
+    ;solicitar del teclado numero 1
+    
+    mov ah, 09
+    lea dx, msj1
+    int 21h
+    mov ah, 01
+    int 21h
+    sub al, 30h
+    mov var1,al
+    
+    ;solicitar del teclado numero 2
+    
+    mov ah, 09
+    lea dx, msj2
+    int 21h
+    mov ah, 01
+    int 21h
+    sub al, 30h
+    mov var2,al
+    
+    ;RESTA
+    mov al,var1
+    sub al,var2
+    mov resultado,al
+    
+    ;mostrando la resta
+    mov ah,09
+    lea dx,msj
+    int 21h
+    mov dl,resultado
+    add dl,30h 
+    mov ah,02
+    int 21h
+    
+    
+    
+    mov ah,09
+    lea dx,textoesc;impresion de cadena y se detiene cuando detecta el --> $
+    int 21h
+       
+    mov ah,01;pausa y captura de datos
+    int 21h
+    
+    cmp al,27; ascii de ESC
+    je Menu
+    
+
+suma
+    mov ah,09
+    lea dx,textosuma;impresion de cadena y se detiene cuando detecta el --> $
+    int 21h
+
+    Sumas proc 
+    printn " "
+    print "numero 1: "
+    call scan_num
+    mov datosuma[0],cl;en la posicion 0 de la variable se almacena el primer valor
+    printn " "
+    print "numero 2: "
+    call scan_num
+    mov datosuma[1],cl;en la posicion 1 de la variable se almacena el primer valor
+    printn " "
+    xor ax,ax ;suma logica exclusiva
+    add al,datosuma[0]
+    add al,datosuma[1]
+    printn " "
+    print "La suma es: "
+    call print_num  
+Sumas endp
+;datos llamados para la suma
+define_print_string
+define_print_num
+define_print_num_uns
+define_scan_num
+    
+    
+    
+    mov ah,09
+    lea dx,textoesc;impresion de cadena y se detiene cuando detecta el --> $
+    int 21h
+    
+    mov ah,01;pausa y captura de datos
+    int 21h
+
+    ;------------------------
+    
+    cmp al,27; ascii de ESC
+    je Menu
+     
+multi:;----------------------------------------------------
+
+    
+    mov ah,09
+    lea dx,textomulti;impresion de cadena y se detiene cuando detecta el --> $
+    int 21h
+    
+lea dx, msj1 ;desplegar numero 1:
+int 21h
+
+mov ah,01h;leer caracter desde el teclado
+int 21h;lee primer caracter
+
+sub al,30h ;restar 30h para obtener el numero
+mov var1,al ;lo guardo en var1  
+
+mov ah,09h
+lea dx, msj2 ;desplegar numero 2:
+int 21h
+
+mov ah,01h;leer caracter desde el teclado
+int 21h;lee primer caracter
+
+sub al,30h ;restar 30h para obtener segundo valor
+mov var2,al ;guardar en var2
+   
+mov ah,09
+    lea dx,salto;impresion de cadena y se detiene cuando detecta el --> $
+    int 21h
+
+mov al,var1
+mov bl,var2
+imul bl
+
+mov bl,10
+
+div bl
+mov bx,ax
+or bx,3030h
+mov ah,02h
+mov dl,bl
+int 21h
+mov ah,02h
+mov dl,bh
+int 21h
+       
+    
+        mov ah,09
+    lea dx,textoesc;impresion de cadena y se detiene cuando detecta el --> $
+    int 21h
+       
+    mov ah,01;pausa y captura de datos
+    int 21h 
+        
+    cmp al,27; ascii de ESC
+    je Menu
+    
+    
+   
+Salir:;---------------------------------------------------------
+    mov ah,04ch
+    int 21h
+    
+    
+    
+end
